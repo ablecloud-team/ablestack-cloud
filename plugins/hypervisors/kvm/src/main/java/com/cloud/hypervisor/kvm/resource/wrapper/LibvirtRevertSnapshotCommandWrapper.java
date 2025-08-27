2ac72bd66ce309e2799325b87624dac2c3e95a8b
Dajeong-Park <ekwjd7182@gmail.com>
2025-08-27 16:01:31 +0900

    Update LibvirtRevertSnapshotCommandWrapper.java

--- plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/resource/wrapper/LibvirtRevertSnapshotCommandWrapper.java
+++ plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/resource/wrapper/LibvirtRevertSnapshotCommandWrapper.java
@@ -211,6 +211,8 @@ public class LibvirtRevertSnapshotCommandWrapper extends CommandWrapper<RevertSn
      * @throws IOException If can't replace the current volume with the snapshot.
      */
     protected void replaceVolumeWithSnapshot(String volumePath, String snapshotPath) throws IOException {
+        logger.info("replaceVolumeWithSnapshot volumePath::: " + volumePath);
+        logger.info("replaceVolumeWithSnapshot snapshotPath::: " + snapshotPath);
         Files.copy(Paths.get(snapshotPath), Paths.get(volumePath), StandardCopyOption.REPLACE_EXISTING);
     }
 }
