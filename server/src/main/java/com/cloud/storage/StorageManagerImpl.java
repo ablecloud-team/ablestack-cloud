83ba85f14fc9832f20c2f7fba032ed786bdd28b2
Wei Zhou <weizhou@apache.org>
2025-09-03 11:12:43 +0200

    Update md5sum to sha512sum (#11134)
    * Update md5sum to sha512sum
    
    * Move SQL to schema-42100to42200.sql

--- server/src/main/java/com/cloud/storage/StorageManagerImpl.java
+++ server/src/main/java/com/cloud/storage/StorageManagerImpl.java
@@ -4060,7 +4060,7 @@ public class StorageManagerImpl extends ManagerBase implements StorageManager, C
                                 }
                             }
                         } catch (Exception e) {
-                            logger.error("Failed to register systemVM template(s)");
+                            logger.error("Failed to register systemVM template(s) due to: ", e);
                         } finally {
                             SystemVmTemplateRegistration.unmountStore(filePath);
                             txn.close();
