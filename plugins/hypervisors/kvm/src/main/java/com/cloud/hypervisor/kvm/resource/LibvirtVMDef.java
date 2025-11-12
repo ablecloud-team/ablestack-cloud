b2d177d774e609f215aa1835e097a48b9eb91546
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2025-11-12 10:52:46 +0900

    Merge pull request #713 from jschoiRR/mold-main#2025
    [Mold Agent] UEFI Lagacy boot모드일 경우 loader format 지정 방식 수정

--- plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/resource/LibvirtVMDef.java
+++ plugins/hypervisors/kvm/src/main/java/com/cloud/hypervisor/kvm/resource/LibvirtVMDef.java
@@ -130,8 +130,8 @@ public class LibvirtVMDef {
         private String _nvram;
         private String _nvramTemplate;
         private boolean iothreads;
-
         private TpmVersion _tpmversion;
+        private String uefiLagacyFormat;
 
         public static final String GUEST_LOADER_SECURE = "guest.loader.secure";
         public static final String GUEST_LOADER_LEGACY = "guest.loader.legacy";
@@ -228,6 +228,10 @@ public class LibvirtVMDef {
             this.iothreads = iothreads;
         }
 
+        public void setUefiLagacyFormat(String uefiLagacyFormat) {
+            this.uefiLagacyFormat = uefiLagacyFormat;
+        }
+
         @Override
         public String toString() {
             if (_type == GuestType.KVM) {
@@ -256,7 +260,7 @@ public class LibvirtVMDef {
                 }
                 if (_loader != null) {
                     if (_bootmode == BootMode.LEGACY) {
-                        guestDef.append("<loader readonly='yes' secure='no' type='pflash' format='qcow2'>" + _loader + "</loader>\n");
+                        guestDef.append("<loader readonly='yes' secure='no' type='pflash' " + uefiLagacyFormat + ">" + _loader + "</loader>\n");
                     } else if (_bootmode == BootMode.SECURE) {
                         guestDef.append("<loader readonly='yes' secure='yes' type='pflash'>" + _loader + "</loader>\n");
                     }
