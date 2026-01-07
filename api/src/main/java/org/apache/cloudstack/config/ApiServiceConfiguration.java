d15e6fea5db300eca446fdba68c70a2e503c72ca
stardom3645 <stardom3645@gmail.com>
2026-01-07 15:39:25 +0900

    wall 대시보드 프로토콜, 포트 변경

--- api/src/main/java/org/apache/cloudstack/config/ApiServiceConfiguration.java
+++ api/src/main/java/org/apache/cloudstack/config/ApiServiceConfiguration.java
@@ -27,7 +27,7 @@ public class ApiServiceConfiguration implements Configurable {public static fina
     public static final ConfigKey<Boolean> ApiSourceCidrChecksEnabled = new ConfigKey<>("Advanced", Boolean.class, "api.source.cidr.checks.enabled",
             "true", "Are the source checks on API calls enabled (true) or not (false)? See api.allowed.source.ip", true, ConfigKey.Scope.Global);
     public static final ConfigKey<String> MonitoringWallPortalProtocol = new ConfigKey<String>("Advanced", String.class, "monitoring.wall.portal.protocol",
-            "http", "Monitoring Service Wall Portal Protocol.(ex: http or https)", true);
+            "https", "Monitoring Service Wall Portal Protocol.(ex: http or https)", true);
     public static final ConfigKey<String> MonitoringWallPortalDomain = new ConfigKey<String>("Advanced", String.class, "monitoring.wall.portal.domain",
             "", "Monitoring Service Wall Portal Domain.(ex: id or domain)", true);
     public static final ConfigKey<String> ApiAllowedSourceIp = new ConfigKey<>(String.class, "api.allowed.source.ip", "Advanced",
@@ -35,7 +35,7 @@ public class ApiServiceConfiguration implements Configurable {public static fina
     public static final ConfigKey<String> ApiAllowedSourceCidr = new ConfigKey<String>("Advanced", String.class, "api.allowed.source.cidr",
             "0", "A cidr setting that allows you to make api calls.", true, ConfigKey.Scope.Account);
     public static final ConfigKey<String> MonitoringWallPortalPort = new ConfigKey<String>("Advanced", String.class, "monitoring.wall.portal.port",
-            "3000", "Monitoring Service Wall Portal Port.(ex:3000)", true);
+            "8081", "Monitoring Service Wall Portal Port.(ex:3000)", true);
     public static final ConfigKey<String> MonitoringWallPortalVmUri = new ConfigKey<String>("Advanced", String.class, "monitoring.wall.portal.vm.uri",
             "/d/uservm?kiosk", "Monitoring Service Wall Portal VM Uri.(ex:/d/uservm?kiosk)", true);
     public static final ConfigKey<String> MonitoringWallPortalHostUri = new ConfigKey<String>("Advanced", String.class, "monitoring.wall.portal.host.uri",
