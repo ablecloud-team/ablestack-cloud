979c859e1289b646bafd630526eebb429c720afd
Dajeong-Park <ekwjd7182@gmail.com>
2026-01-16 12:57:42 +0900

    빌드오류 수정

--- engine/schema/src/main/java/org/apache/cloudstack/backup/dao/BackupDaoImpl.java
+++ engine/schema/src/main/java/org/apache/cloudstack/backup/dao/BackupDaoImpl.java
@@ -233,4 +233,12 @@ public class BackupDaoImpl extends GenericDaoBase<BackupVO, Long> implements Bac
         response.setObjectName("backup");
         return response;
     }
+
+    public static class SumCount {
+        public long sum;
+        public long count;
+
+        public SumCount() {
+        }
+    }
 }
