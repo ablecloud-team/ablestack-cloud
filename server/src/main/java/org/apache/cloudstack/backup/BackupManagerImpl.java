b3c7a43dfcaf3410b87d7134e51dbfe90b4c3592
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2026-01-16 13:04:59 +0900

    빌드 오류 수정

--- server/src/main/java/org/apache/cloudstack/backup/BackupManagerImpl.java
+++ server/src/main/java/org/apache/cloudstack/backup/BackupManagerImpl.java
@@ -24,6 +24,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.TimeZone;
 import java.util.Timer;
 import java.util.TimerTask;
