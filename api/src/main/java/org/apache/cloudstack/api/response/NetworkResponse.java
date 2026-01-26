f6fa31cfb41b91a1e0a4e7378e8f54cc327cd773
JS Choi <77760789+jschoiRR@users.noreply.github.com>
2026-01-26 11:48:40 +0900

    병합 누락 부분 수정

--- api/src/main/java/org/apache/cloudstack/api/response/NetworkResponse.java
+++ api/src/main/java/org/apache/cloudstack/api/response/NetworkResponse.java
@@ -211,14 +211,6 @@ public class NetworkResponse extends BaseResponseWithAssociatedNetwork implement
     @Param(description = "Name of the VPC to which this Network belongs", since = "4.15")
     private String vpcName;
 
-    @SerializedName(ApiConstants.ASSOCIATED_NETWORK_ID)
-    @Param(description = "The ID of the Network associated with this Network")
-    private String associatedNetworkId;
-
-    @SerializedName(ApiConstants.ASSOCIATED_NETWORK)
-    @Param(description = "The name of the Network associated with this Network")
-    private String associatedNetworkName;
-
     @SerializedName(ApiConstants.TUNGSTEN_VIRTUAL_ROUTER_UUID)
     @Param(description = "Tungsten-Fabric virtual router the Network belongs to")
     private String tungstenVirtualRouterUuid;
@@ -359,10 +351,6 @@ public class NetworkResponse extends BaseResponseWithAssociatedNetwork implement
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setBroadcastDomainType(String broadcastDomainType) {
